bd5547d32d4576e6d8687ce8f1c857f1bc231357
Kieran <kieran.broggi@escen.fr>
2025-07-13 05:50:49 +0000

    feat: add stg_sale_database__order_items.sql

---
+++ models/staging/sale_database/stg_sale_database__order_items.sql
@@ -0,0 +1,10 @@
+select
+    concat(order_id, '_', item_id) AS order_item_id,
+    order_id,
+    item_id,
+    product_id,
+    quantity,
+    list_price,
+    discount,
+    (list_price * quantity)*(1-discount) as total_order_items_amount
+from {{ source('sale_database', 'order_items') }}
